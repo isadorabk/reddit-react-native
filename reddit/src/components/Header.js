801208a9b4c36e93ea1a66f34b5ddae64ac7c89d
Isadora Bassetto Kwiatkowski <isadorabk@gmail.com>
2018-09-11 11:47:30 +0200

    fix: styling Header

--- reddit/src/components/Header.js
+++ reddit/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Header = ({ headerText }) => {
   const { textStyle, viewStyle } = styles;
@@ -11,22 +11,23 @@ const Header = ({ headerText }) => {
     );
 };
 
-const styles = {
+const styles = StyleSheet.create({
   viewStyle: {
     backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 80,
-    paddingTop: 15,
+    height: 70,
+    paddingTop: 10,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     elevation: 2,
-    position: 'relative'
+    position: 'relative',
+    paddingBottom: 5
   },
   textStyle: {
     fontSize: 20
   }
-};
+});
 
 export default Header;
